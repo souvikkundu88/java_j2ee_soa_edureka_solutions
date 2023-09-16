e071ff5cc1494f6bedb064ef4751c134037f00b4
Souvik Kundu <kivtech88@gmail.com>
2023-09-17 03:27:11 +0530

    commit

---
+++ src/java_module2/testing_checking_code.java
@@ -0,0 +1,14 @@
+package java_module2;
+
+public class testing_checking_code {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+		int[] marks = {92, 87, 99, 65};
+		for(int mark: marks) {
+			System.out.print(mark + " ");
+		}
+	}
+
+}
