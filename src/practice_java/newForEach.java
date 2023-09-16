47b82b5ad5a4c611e2460c69780c80be58a7aac4
Souvik Kundu <kivtech88@gmail.com>
2023-09-17 04:31:57 +0530

    commit

--- src/practice_java/newForEach.java
+++ src/practice_java/newForEach.java
@@ -1,12 +1,12 @@
-package java_module2;
+package practice_java;
 
-public class testing_checking_code {
+public class newForEach{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world");
 		int[] marks = {92, 87, 99, 65};
-		for(int mark: marks) {
+		for(int mark: marks) { // this is the new for-each loop in java
 			System.out.print(mark + " ");
 		}
 	}
