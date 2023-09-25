1d99b842cf4da024f0f4e62ebafbdef939f57507
Souvik Kundu <kivtech88@gmail.com>
2023-09-26 01:58:14 +0530

    Update questionOneSolution.java

--- src/java_module2_assignment_solution/questionOneSolution.java
+++ src/java_module2_assignment_solution/questionOneSolution.java
@@ -3,7 +3,7 @@
           from the user and store them in three arrays. Pass these arrays to a function display() as
           arguments.
           
-          Disply in this following format
+          Display in this following format
           *************************************************
           ID				Name				Salary
           *************************************************
@@ -33,19 +33,36 @@ public class questionOneSolution {
 	static void employeeInformation(int[] employeeIDs, String[] employeeNames, int[] employeeSalaries) {
 		int i;
 		for(i=0; i<5; i++) {
+			
+			// input ID
 			System.out.println("Enter Employee ID: ");
 			employeeIDs[i] = sc.nextInt();
+			
+			// input name
 			System.out.println("Enter Employee Name: ");
 			employeeNames[i] = sc.nextLine();
+			sc.nextLine();
+			
+			// input salary
 			System.out.println("Enter EmployeeSalary: ");
 			employeeSalaries[i] = sc.nextInt();
+			sc.nextLine();
+			
 	 }
-	 sc.close();
-  }
+	 
+	sc.close();
+}
 	
 	// pass the arrays to a display function
 	static void displaySalaryChart(int[] employeeIDs, String[] employeeNames, int[] employeeSalaries) {
-		
+		System.out.println("*************************************************");
+		System.out.println("ID \t\t\t\t Name \t\t\t\t Salary");
+		System.out.println("*************************************************");
+		int j;
+		for(j=0; j < 5; j++) {
+			System.out.println(employeeIDs[j] + "\t\t\t\t" + employeeNames[j] + "\t\t\t\t" + employeeSalaries[j]);
+		}
+		System.out.println("*************************************************");
 	}
 	
 }
