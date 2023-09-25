6cf35d39ec73371ac32340edda83b945db1e8e09
Souvik Kundu <kivtech88@gmail.com>
2023-09-26 00:50:09 +0530

    commit

---
+++ src/java_module2_assignment_solution/questionOneSolution.java
@@ -0,0 +1,29 @@
+/*
+		 *1. Write a program to accept 5 employee IDs and the corresponding names and their salaries
+          from the user and store them in three arrays. Pass these arrays to a function display() as
+          arguments.
+          
+          Disply in this following format
+          *************************************************
+          ID				Name				Salary
+          *************************************************
+          00				Sk					10000
+          01				Rk					15000
+          02				Yk					20000
+          04				Ik					30000
+          05				Ak					40000
+          *************************************************
+*/
+package java_module2_assignment_solution;
+
+import java.util.Scanner;
+
+public class questionOneSolution {
+	
+	static Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+				
+	}
+
+}
