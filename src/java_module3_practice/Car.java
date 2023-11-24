27be7026cff4b7215fd55ad69beea8a3fea7a4ef
Souvik Kundu <kivtech88@gmail.com>
2023-11-24 11:24:27 +0530

    Update Car.java

--- src/java_module3_practice/Car.java
+++ src/java_module3_practice/Car.java
@@ -2,10 +2,11 @@ package java_module3_practice;
 import java.util.Scanner;
 
 class Car {
+	/*
 	public Car() {
 		System.out.println("base class");
 	}
-	
+	*/
 	void display() {
 		System.out.println("generic cars");
 	}
@@ -28,6 +29,17 @@ class BMW extends Car {
 class PolymorphismExample {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		System.out.println
-	}
+		System.out.println("Enter the name of car:");
+		String carType = scanner.next();
+		Car car; // reference of base class
+		if (carType.equalsIgnoreCase("Merc")) {
+            car = new Mercedes(); // Creating object of Mercedes
+        } else if (carType.equalsIgnoreCase("Bmw")) {
+            car = new BMW(); // Creating object of BMW
+        } else {
+            car = new Car(); // Default Car object
+        }
+
+        car.display(); // Calls the display() method based on the object created (runtime polymorphism)
+    }
 }
