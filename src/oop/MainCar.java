d285dd962487890363e60a1ebb75a3b9d3b2701a
Souvik Kundu <kivtech88@gmail.com>
2023-12-05 15:11:31 +0530

    Update MainCar.java

--- src/oop/MainCar.java
+++ src/oop/MainCar.java
@@ -11,16 +11,50 @@ class BaseCar {
 		this.manufacturer = manufacturer;
 	}
 	
+	// print car model and manufacturer
 	public void printDetails() {
-		System.out.println();
+		System.out.println("Model of " + getClass().getSimpleName() + " is " + model); // print model
+		System.out.println("Manufacturer of " + getClass().getSimpleName() + " is " + manufacturer); // print manufacturer
 	}
 }
 
+// interface for type of sedan
+interface IsSedan {
+	int bootSpace = 456;
+	void bootSpace(); 
+}
+
+// child class / sub-class extends & implements
+class Elantra extends BaseCar implements IsSedan {
+	private String variant;
+	
+	public Elantra (int model, String variant) {
+		super(model, "Hyundai");
+		this.variant = variant;
+	}
+	
+	@Override
+	public void bootSpace() {
+		System.out.println("bootspace of Elantra: " + IsSedan.bootSpace + " liters");
+	}
+	
+	@Override
+	public void printDetails() {
+		super.printDetails();
+		System.out.println("The variant of Elantra is " + variant);
+	}
+}
+
+// main class of the car
 public class MainCar {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Elantra sport = new Elantra(2019, "Sport");
+		Elantra eco = new Elantra(2018, "Eco");
+		
+		sport.printDetails();
+		sport.bootSpace();
+		System.out.println("");
+		eco.printDetails();
 	}
-
 }
