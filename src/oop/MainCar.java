f976768b3792ce320a30624182f7699178d83ddd
Souvik Kundu <kivtech88@gmail.com>
2023-12-05 14:22:01 +0530

    interface oop example

---
+++ src/oop/MainCar.java
@@ -0,0 +1,26 @@
+package oop;
+
+class BaseCar {
+	// instance variables
+	private int model;
+	private String manufacturer;
+	
+	// default constructor of BaseCar
+	public BaseCar(int model, String manufacturer) {
+		this.model = model;
+		this.manufacturer = manufacturer;
+	}
+	
+	public void printDetails() {
+		System.out.println();
+	}
+}
+
+public class MainCar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
