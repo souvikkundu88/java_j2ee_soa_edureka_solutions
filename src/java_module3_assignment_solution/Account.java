3c7a84a5ae11a22ce449fa9339e226a8450f43ac
Souvik Kundu <kivtech88@gmail.com>
2023-12-08 13:03:51 +0530

    Update Account.java

--- src/java_module3_assignment_solution/Account.java
+++ src/java_module3_assignment_solution/Account.java
@@ -2,5 +2,15 @@ package java_module3_assignment_solution;
 import java.util.Scanner;
 
 abstract class Account {
+	// common properties
+	protected int accountNumber;
+	protected String name;
+	protected int amount;
 	
+	// parameterized constructor
+	Account(int accountNumber, String name, double amount) {
+		this.accountNumber = accountNumber;
+		this.name = name;
+		this.amount = amount;
+	}
 }
